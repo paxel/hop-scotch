dd13bb61d5c304f5dd08ad0c8d3c6e2c8d8e80c7
Patrick Zimmer <taum@tuta.io>
2025-03-27 18:59:13 +0100

    Add CreatorQueryBuilder

--- src/main/java/paxel/hopscotch/impl/enrichment/CreatorQueryBuilderImpl.java
+++ src/main/java/paxel/hopscotch/impl/enrichment/CreatorQueryBuilderImpl.java
@@ -6,13 +6,15 @@ import paxel.hopscotch.api.enrichment.CreatorQueryBuilder;
 import java.util.List;
 
 /**
- *
+ * Implements the {@link CreatorQueryBuilder} which reduces the available enrichments depending on the Creator.
  */
 public class CreatorQueryBuilderImpl implements CreatorQueryBuilder {
     private final List<EnrichmentImpl> enrichments;
 
     /**
-     * @param enrichments
+     * Constructs an instance with the remaining {@link Enrichment}s
+     *
+     * @param enrichments The remaining enrichments.
      */
     public CreatorQueryBuilderImpl(List<EnrichmentImpl> enrichments) {
         this.enrichments = enrichments;
@@ -21,6 +23,6 @@ public class CreatorQueryBuilderImpl implements CreatorQueryBuilder {
 
     @Override
     public CreatorQueryBuilder matchExact(Creator creator) {
-        return null;
+        return new CreatorQueryBuilderImpl(enrichments.stream().filter(e -> e.creator().equals(creator)).toList());
     }
 }
